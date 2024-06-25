594e88b75c72abcf11d44294b8153e03ecce5268
Stas Ukolov <ukoloff@gmail.com>
2024-06-25 10:23:39 +0500

    Watch fs

--- push/index.js
+++ push/index.js
@@ -1,8 +1,13 @@
 const os = require('node:os')
 const path = require('node:path')
 const cp = require('node:child_process')
+const tm = require('node:timers/promises')
+const c7r = require('chokidar')
 
 const cwd = path.join(__dirname, '../nc.ekb.ru/omz/service')
+
+c7r.watch(path.join(cwd, 'sigur'))
+  .on('all', watcher)
 
 function rSync() {
   return new Promise(run)
@@ -25,6 +30,35 @@ function rSync() {
   }
 }
 
-rSync()
-  .then(console.log)
-  .catch(console.log)
+var state = 0
+
+function watcher(event, path) {
+  switch (state) {
+    case 0: //  Waiting for event(s)
+      state = 1
+      tm.setTimeout(100)
+        .then(fire)
+      break
+    case 2: // Running rSync
+      state = 3
+  }
+}
+
+var startAt
+function fire() {
+  state = 2
+  startAt = new Date()
+  console.log(`<rsync at="${startAt.toLocaleString()}">`)
+  rSync()
+    .then(synced)
+    .catch(console.log)
+}
+
+function synced() {
+  console.log(`</rsync elapsed="${(new Date() - startAt) / 1000}">`)
+  if (state == 3) {
+    fire()
+    return
+  }
+  state = 0
+}
