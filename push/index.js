533c33cdb14eef5d198e76f770725a92d84c6df4
Stas Ukolov <ukoloff@gmail.com>
2024-06-25 09:44:55 +0500

    Catch errors

--- push/index.js
+++ push/index.js
@@ -1,5 +1,5 @@
-const path = require('path')
-const cp = require('child_process')
+const path = require('node:path')
+const cp = require('node:child_process')
 
 const cwd = path.join(__dirname, '../nc.ekb.ru/omz/service')
 
@@ -7,18 +7,22 @@ function rSync() {
   return new Promise(run)
 
   function run(resolve, reject) {
-    cp.spawn('rsync', [
+    var z = cp.spawn('rsync', [
       '-e',
       'C:\\Users\\s.ukolov\\scoop\\apps\\cwrsync\\6.3.0\\bin\\ssh.exe -o HostKeyAlgorithms=+ssh-dss -o PubkeyAcceptedKeyTypes=+ssh-rsa',
       '-a',
+      '--delete',
       'sigur',
       'stas@10.33.10.104:dev/',
     ], {
       cwd: cwd,
       stdio: 'inherit'
     })
+      .on('error', reject)
+      .on('close', resolve)
   }
 }
 
-
 rSync()
+  .then(console.log)
+  .catch(console.log)
