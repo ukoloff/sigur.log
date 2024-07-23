193a178b7069762dae1e226c8336ae795333bb98
Stas Ukolov <ukoloff@gmail.com>
2024-07-23 10:16:09 +0500

    Use GUID for AD match

--- nc.ekb.ru/omz/service/sigur/lib/user.php
+++ nc.ekb.ru/omz/service/sigur/lib/user.php
@@ -8,26 +8,22 @@ $CFG->sigur->h = $h;
 function sigur_UID($u = '')
 {
   global $CFG;
-  if (!$u) {
-    $u = $CFG->u;
-  }
-  $dn = user2dn($u);
-  if (!$dn) return;
-  $dn = substr(utf2str($dn), 0, -1 - strlen($CFG->AD->baseDN));
+  if (!$u) $u = $CFG->u;
+  $g = getEntry(user2dn($u), 'objectguid');
+  $g = bin2hex($g['objectguid'][0]);
 
   $s = $CFG->sigur->h->prepare(
     <<<SQL
-    Select
-      ID
-    From
-      personal
-    Where
-      USER_ENABLED
-    And
-      AD_USER_DN=?
+      Select
+        ID
+      From
+        personal
+      Where
+        USER_ENABLED
+        And EXTID = ?
 SQL
   );
-  $s->execute(array($dn));
+  $s->execute(array($g));
   $row = $s->fetch();
   if (!$row) return;
   return $row[0];
@@ -46,4 +42,3 @@ function sigurUID($u = '')
 }
 
 $CFG->sigur->uid = sigurUID();
-?>
