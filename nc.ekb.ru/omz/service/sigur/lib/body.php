3c78b43118608ea4ba60b68ce8f78ba3ff7e0f63
Stas Ukolov <ukoloff@gmail.com>
2024-07-24 14:38:10 +0500

    Count active departments

--- nc.ekb.ru/omz/service/sigur/lib/body.php
+++ nc.ekb.ru/omz/service/sigur/lib/body.php
@@ -15,16 +15,18 @@ echo htmlspecialchars($row[0]);
 <form method='POST' target='inner'>
   <label>
     С даты
-    <input type='date' name='dA' required />
+    <input type='date' name='dA' required value='2024-07-01' />
   </label>
   <label>
     По дату
-    <input type='date' name='dZ' required />
+    <input type='date' name='dZ' required value='2024-07-31' />
   </label>
   <input type='submit' value=' Отправить! ' />
+  <fieldset><legend>Подразделения (<span></span>)</legend>
   <?
   LoadLib('./depts');
   renderDepts(loadDepts());
   ?>
+  </fieldset>
 </form>
 <iframe name=inner></iframe>
--- nc.ekb.ru/omz/service/sigur/sigur.js
+++ nc.ekb.ru/omz/service/sigur/sigur.js
@@ -1,6 +1,7 @@
 setTimeout(function () {
   document.getElementById('/*')
     .addEventListener('click', clicker)
+  updateCounts()
 }, 100)
 
 function clicker(ev) {
@@ -37,6 +38,7 @@ function clickExpander(a) {
 function clickCB(cb) {
   cb.blur()
   var div = document.getElementById(cb.name.replace('?', '/'))
+  setTimeout(updateCounts)
   if (!div) return
   var cbs = div.getElementsByTagName('input')
   for (var i = cbs.length - 1; i >= 0; i--) {
@@ -44,5 +46,24 @@ function clickCB(cb) {
     if (z.disabled) continue
     z.checked = cb.checked
   }
+}
 
+function countDepts() {
+  var cbs = document.getElementById('/*').querySelectorAll('input[type=checkbox]')
+  var found = 0
+  var checked = 0
+  for (var i = cbs.length - 1; i >= 0; i--) {
+    var z = cbs[i]
+    if (z.disabled) continue
+    found++
+    if (z.checked) checked++
+  }
+  return [checked, found]
+}
+
+function updateCounts() {
+  var counts = countDepts()
+  document.querySelector('form input[type=submit]').disabled = counts[0] == 0
+  var s = document.querySelector('fieldset > legend > span')
+  if (s) s.innerText = counts.join('/')
 }
