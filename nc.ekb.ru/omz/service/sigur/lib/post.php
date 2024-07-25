01d005afdce03623cc608deb468ea401b7b45c5a
Stas Ukolov <ukoloff@gmail.com>
2024-07-25 08:54:40 +0500

    Write Excel

--- nc.ekb.ru/omz/service/sigur/lib/post.php
+++ nc.ekb.ru/omz/service/sigur/lib/post.php
@@ -1,9 +1,46 @@
 <?
-header('Content-disposition: attachment; filename="a.xlsx"');
+header("Content-Type: application/vnd.ms-excel");
+header('Content-disposition: attachment; filename="a.xls"');
 ?>
-Hello, world!
-<pre>
+<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel"
+  xmlns="http://www.w3.org/TR/REC-html40">
+
+<head>
+  <!--[if gte mso 9]><xml>
+<x:ExcelWorkbook>
+<x:ExcelWorksheets>
+<x:ExcelWorksheet>
+<x:Name>Отчёт Sigur</x:Name>
+<x:WorksheetOptions>
+<x:NoSummaryRowsBelowDetail/>
+<x:NoSummaryColumnsRightDetail/>
+</x:WorksheetOptions>
+</x:ExcelWorksheet>
+</x:ExcelWorksheets>
+</x:ExcelWorkbook>
+</xml><![endif]-->
+  <meta http-equiv="Content-Type" content='text/html; charset=windows-1251' />
+</head>
+
+<body>
+  <table>
+
+    <tr>
+      <td align="center"><b>1</b></td>
+      <td>2</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>Превед</td>
+      <td>Medved</td>
+      <td>A3</td>
+    </tr>
+
+  </table>
+</body>
+
+</html>
+
 <?
-print_r($_REQUEST);
 exit();
 ?>
