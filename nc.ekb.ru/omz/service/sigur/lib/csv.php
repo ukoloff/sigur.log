cd4681ef127cb3b0af4c40f95d5168dc9ffdc3cf
Stas Ukolov <ukoloff@gmail.com>
2024-07-25 11:18:17 +0500

    Drop last comma

--- nc.ekb.ru/omz/service/sigur/lib/csv.php
+++ nc.ekb.ru/omz/service/sigur/lib/csv.php
@@ -4,18 +4,18 @@ header("Content-Type: text/csv");
 $rows = 0;
 while ($row = $CFG->sigur->data->fetchObject()):
   if ($rows == 0):
-    echo "¹,";
+    echo "¹";
     foreach ($row as $k => $v):
-      echo "$k,";
+      echo ",$k";
     endforeach;
     echo "\n";
   endif;
   $rows++;
-  echo "$rows,";
+  echo "$rows";
   foreach ($row as $k => $v):
     if (preg_match('/[,"]/', $v))
       $v = '"' . str_replace('"', '""', $v) . '"';
-    echo $v, ",";
+    echo ",$v";
   endforeach;
   echo "\n";
 endwhile;
