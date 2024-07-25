7ea4f22bdf1757cc19aec34a53b72b3982591e53
Stas Ukolov <ukoloff@gmail.com>
2024-07-25 11:09:59 +0500

    Write CSV

---
+++ nc.ekb.ru/omz/service/sigur/lib/csv.php
@@ -0,0 +1,19 @@
+<?
+header("Content-Type: text/csv");
+
+$rows = 0;
+while ($row = $CFG->sigur->data->fetchObject()):
+  if ($rows == 0):
+    echo "¹,";
+    foreach ($row as $k => $v):
+      echo "$k,";
+    endforeach;
+    echo "\n";
+  endif;
+  $rows++;
+  echo "$rows,";
+  foreach ($row as $k => $v):
+    echo $v, ",";
+  endforeach;
+  echo "\n";
+endwhile;
