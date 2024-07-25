8366c4aafd9a596f919c1c0194662ddd43c7e706
Stas Ukolov <ukoloff@gmail.com>
2024-07-25 08:09:12 +0500

    Fetch date range

--- nc.ekb.ru/omz/service/sigur/lib/body.php
+++ nc.ekb.ru/omz/service/sigur/lib/body.php
@@ -15,24 +15,38 @@ else:
 endif;
 echo "<hr>";
 
-$d = new DateTime();
-$d->modify('first day of this month');
-
 function d2s($date)
 {
   return preg_replace('/T.*/', '', $date->format('c'));
 }
 
+$d = new DateTime();
+$d0 = d2s($d);
+$d->modify('first day of this month');
+$d1 = d2s($d);
+
+$s = $CFG->sigur->h->prepare(<<<SQL
+  select
+    cast(max(LOGTIME) as date) as max,
+    cast(min(LOGTIME) as date) as min
+  from
+    `tc-db-log`.logs
+SQL
+);
+$s->execute();
+$dates = $s->fetchObject();
+$minmax = "min=$dates->min max=$dates->max";
+
 ?>
 <form method='POST' target='inner'>
   <label>
     С даты
-    <input type='date' name='dA' required value='<?= d2s($d) ?>' />
+    <input type='date' name='dA' <?= $minmax ?> required value='<?= d2s($d) ?>' />
   </label>
   <? $d->modify('last day of this month'); ?>
   <label>
     По дату
-    <input type='date' name='dZ' required value='<?= d2s($d) ?>' />
+    <input type='date' name='dZ' <?= $minmax ?> required value='<?= d2s($d) ?>' />
   </label>
   <input type='submit' value=' Сформировать отчёт! ' />
   <fieldset>
