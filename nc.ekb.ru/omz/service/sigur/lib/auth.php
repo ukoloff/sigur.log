993c813ff03f69237cf54d8652c28fb1435d4307
Stas Ukolov <ukoloff@gmail.com>
2024-08-05 11:47:55 +0500

    Fiddle buttons' text

--- nc.ekb.ru/omz/service/sigur/lib/auth.php
+++ nc.ekb.ru/omz/service/sigur/lib/auth.php
@@ -1,23 +1,33 @@
 Требуется авторизация!
 
 <table width="100%">
-  <tr>
+  <tr valign="top">
     <td width="50%" align="center">
       <form action="https://ad.ekb.ru/auth/krb/">
         <input type="hidden" name="auth" />
         <button type="submit">
-          Авторизация в<br>
-          Kerberos!
+          Войти с текущей<br>
+          учётной записью
         </button>
+        <p>
+        <i>Так, как Вы вошли в компьютер</i>
       </form>
     </td>
     <td width="50%" align="center">
       <form action="./">
         <input type="hidden" name="auth" value="AD" />
         <button type="submit">
-          Авторизация в<br>
-          AD!
+          <? if ($CFG->u): ?>
+            Войти как <tt><?= htmlspecialchars($CFG->u) ?></tt><br>
+            <? $e = getEntry(user2dn($CFG->u)); ?>
+            <i><?= utf2str($e['cn'][0]) ?></i>
+          <? else: ?>
+            Ввести учётную запись<br>
+            и пароль
+          <? endif; ?>
         </button>
+        <p>
+        <i>Учётная запись вводится <b>без</b> домена (<s>OMZGLOBAL\</s>)</i>
       </form>
     </td>
   </tr>
