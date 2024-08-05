f943cb8f3d60d4b27b49f0bc303ef78a32a7df9c
Stas Ukolov <ukoloff@gmail.com>
2024-08-05 13:19:36 +0500

    Wrap with FieldSets

--- nc.ekb.ru/omz/service/sigur/lib/main.php
+++ nc.ekb.ru/omz/service/sigur/lib/main.php
@@ -54,26 +54,32 @@ $minmax = "min=$dates->min max=$dates->max";
   <table cellspacing="0">
     <tr>
       <td align="right">
-        <label>
-          С
-          <input type='date' name='dA' <?= $minmax ?> required value='<?= $d1 ?>' />
-        </label>
-        <br />
-        <label>
-          По
-          <input type='date' name='dZ' <?= $minmax ?> required value='<?= $d0 ?>' />
-        </label>
+        <fieldset>
+          <legend><small>Даты</small></legend>
+          <label>
+            С
+            <input type='date' name='dA' <?= $minmax ?> required value='<?= $d1 ?>' />
+          </label>
+          <br />
+          <label>
+            По
+            <input type='date' name='dZ' <?= $minmax ?> required value='<?= $d0 ?>' />
+          </label>
+        </fieldset>
       </td>
-      <td width="30%">
-        <label>
-          <input type="radio" name="format" value="xls" checked>
-          XLS<sup title="См. примечание внизу страницы">*</sup>
-        </label>
-        <br />
-        <label>
-          <input type="radio" name="format" value="csv">
-          CSV
-        </label>
+      <td valign="top">
+        <fieldset>
+          <legend><small>Формат файла</small></legend>
+          <label>
+            <input type="radio" name="format" value="xls" checked>
+            XLS<sup title="См. примечание внизу страницы">*</sup>
+          </label>
+          <br />
+          <label>
+            <input type="radio" name="format" value="csv">
+            CSV
+          </label>
+        </fieldset>
       </td>
       <td>
         <button type='submit' disabled>
