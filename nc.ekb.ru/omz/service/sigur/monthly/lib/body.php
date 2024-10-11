347c9cf480b559463f08312c13749e52e7ad10d6
Stas Ukolov <ukoloff@gmail.com>
2024-10-11 10:24:52 +0500

    Build tree of (sub)depts

---
+++ nc.ekb.ru/omz/service/sigur/monthly/lib/body.php
@@ -0,0 +1,2 @@
+<?
+LoadLib('depts');
---
+++ nc.ekb.ru/omz/service/sigur/monthly/lib/depts.php
@@ -0,0 +1,51 @@
+<?
+global $CFG;
+LoadLib('/dc/db/sigur.connect');
+$h = $CFG->sigur;
+unset($CFG->sigur);
+$CFG->sigur->h = $h;
+
+$dept_id = 6316; // Отдел организации и нормирования труда
+// $dept_id = 6216; // Уралхиммаш
+
+$q = $h->prepare(<<<SQL
+  with recursive depts as(
+    select
+      ID,
+      PARENT_ID,
+      NAME
+    from
+      personal
+    where
+      `TYPE` = 'DEP'
+      and STATUS = 'AVAILABLE'
+  ),
+  base as(
+    select
+      *
+    from
+      depts
+    where
+      id = ? -- 6216 = Уралхиммаш
+  ),
+  tree as(
+    select
+      *
+    from
+      base
+    union all
+    select
+      D.*
+    from
+      tree P
+      join depts D on P.ID = D.PARENT_ID
+  )
+  select
+    ID
+  from
+    tree
+SQL
+);
+$q->execute(array($dept_id));
+$ids = $q->fetchAll(PDO::FETCH_COLUMN);
+print_r($ids);
