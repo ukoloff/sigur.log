7d7c8302bcb71d4c615054c629d4e790b4817502
Stas Ukolov <ukoloff@gmail.com>
2024-11-01 09:53:58 +0500

    Disable TLS verify

--- nc.ekb.ru/omz/service/sigur/lib/kerberos.php
+++ nc.ekb.ru/omz/service/sigur/lib/kerberos.php
@@ -5,7 +5,7 @@ $c = curl_init('https://ad.ekb.ru/auth/krb/be/');
 curl_setopt($c, CURLOPT_POST, 1);
 curl_setopt($c, CURLOPT_POSTFIELDS, "tIcKeT=" . urlencode($t));
 curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-// curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
 // curl_setopt($c, CURLOPT_CAINFO, "/var/www/net.ekb.ru/ssl/ca.crt");
 $Res = curl_exec($c);
 $Res = json_decode($Res);
