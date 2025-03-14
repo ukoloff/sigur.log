c09a719075ce11e3d00046b797f68e4fa39b1e3c
Stas Ukolov <ukoloff@gmail.com>
2025-03-14 09:41:25 +0500

    Create image

--- nc.ekb.ru/omz/dc/user/lib/jpg10k.php
+++ nc.ekb.ru/omz/dc/user/lib/jpg10k.php
@@ -4,4 +4,18 @@ function jpgShrink10k($jpg, $limit = 10000)
 {
   if (strlen($jpg) <= $limit)
     return $jpg;
+
+  // Convert to image
+  $tmp = tempnam('/var/tmp', 'voxr');
+  $f = fopen($tmp, 'w');
+  fwrite($f, $jpg);
+  fclose($f);
+  $i = imagecreatefromjpeg($tmp);
+  unlink($tmp);
+
+  $Sz[0] = imagesx($i);
+  $Sz[1] = imagesy($i);
+  $idx = (int)($Sz[1] > $Sz[0]);  // 0 or 1
+
+  print_r($Sz);
 }
