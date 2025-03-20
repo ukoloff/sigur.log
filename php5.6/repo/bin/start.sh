28edec1b64c70a49066006805df9892e3e804747
Stas Ukolov <ukoloff@gmail.com>
2025-03-20 19:25:58 +0500

    Initial clone repo

--- php5.6/repo/bin/start.sh
+++ php5.6/repo/bin/start.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+# https://superuser.com/a/1361548/2436173
+if test -n "$(find ./ -maxdepth 0 -empty)"
+then
+  git clone $GIT_REPO --branch $GIT_BRANCH .
+fi
+
+git pull --ff-only
+
 exec tail -f /dev/null
